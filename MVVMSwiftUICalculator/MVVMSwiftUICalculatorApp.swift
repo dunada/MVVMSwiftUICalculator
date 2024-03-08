e266e95ccd9ee927366dbd39024f4edd2f21ff4d
Roy <eduardo_rodrigues@epam.com>
2024-03-08 15:13:10 -0300

    Initial Commit

---
+++ MVVMSwiftUICalculator/MVVMSwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUICalculatorApp.swift
+//  MVVMSwiftUICalculator
+//
+//  Created by Eduardo Rodrigues on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
